8048f0cee210a97d06e7907496824cc25f92ee32
Antonio <43041252+ajrjunior@users.noreply.github.com>
2020-11-18 14:21:18 -0300

    Delete cores.py

--- Dicionarios/cores.py
+++
@@ -1,7 +0,0 @@
-cores = {'verde': 'green', 'vermelho': 'red', 'preto': 'black', 'branco': 'white'}
-cor = input('Escolha a cor que deseja traduzir: ').lower()
-traducao = cores.get(cor, 'Esta cor não consta no meu dicionário')
-#print(traducao)
-for x in cores:
-    print(x)
-    #print(x+":"+cores[x])
