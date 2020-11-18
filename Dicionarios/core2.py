941f90860073c3f338d4c30c3c3407a107aab97c
Antonio <43041252+ajrjunior@users.noreply.github.com>
2020-11-18 14:20:36 -0300

    Delete core2.py

--- Dicionarios/core2.py
+++
@@ -1,4 +0,0 @@
-cores = {'preto':'black','branco':'white','amarelo':'yello','verde':'green'}
-escolha = input('Escolha a Cor para ser traduzida: ').lower()
-traducao = cores.get(escolha, 'Está cor não existe !')
-print(traducao)
